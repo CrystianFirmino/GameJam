4abbaababa85c6a366f336ca145336ea0e9babcb
CrystianFirmino <crystian.s.f@gmail.com>
2019-08-30 04:47:35 -0300

    v1.1

---
+++ Assets/Guardinha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guardinha : MonoBehaviour
+{
+    public int vel = 4;
+
+    float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+    void Update()
+    {
+        transform.position += Vector3.left * vel * Time.deltaTime;
+        if (Time.time - startTime >= 20)
+            Destroy(gameObject);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
