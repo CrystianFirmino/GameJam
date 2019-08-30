95bd623e1100ecd80588989b8360ea69c87dadd9
CrystianFirmino <crystian.s.f@gmail.com>
2019-08-30 06:23:04 -0300

    v2.0

--- Assets/Inimigo/Inimigo1.cs
+++ Assets/Inimigo/Inimigo1.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Inimigo1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int vida = 3;
+    public int freqTiros = 100;
+    public GameObject bullet;
 
-    // Update is called once per frame
+    int hitCount = 0;
+
+    float balaTime;
+
     void Update()
     {
-        
+        if (hitCount >= vida)
+            Destroy(gameObject);
+        if (Random.Range(0, freqTiros) == 2 & Time.time - balaTime >= 1)
+            AttackL();
+        else if (Random.Range(0, freqTiros) == 4 & Time.time - balaTime >= 1)
+            AttackH();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bala")
+        {
+            hitCount += 1;
+        }
+    }
+
+    public void AttackL()
+    {
+        //atack
+        Instantiate(bullet, transform.position + (Vector3.left) + (Vector3.down * 4 / 6), Quaternion.identity);
+        balaTime = Time.time;
+    }
+    public void AttackH()
+    {
+        //atack
+        Instantiate(bullet, transform.position + (Vector3.left) + (Vector3.up * 4 / 6), Quaternion.identity);
+        balaTime = Time.time;
     }
 }
---
+++ Assets/Inimigo/Inimigo2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo2 : MonoBehaviour
+{
+    public int vel = 1;
+
+    float startTime;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+    void Update()
+    {
+        transform.position += Vector3.left * vel * Time.deltaTime;
+
+        if (Time.time - startTime >= 20) //Mudar para quando sair da tela
+            Destroy(gameObject);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
