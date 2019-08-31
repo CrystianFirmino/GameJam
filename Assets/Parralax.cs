00230b1ae0ee35e782c8115af3ebe1833c750fc1
CrystianFirmino <crystian.s.f@gmail.com>
2019-08-31 08:57:27 -0300

    v7.2

---
+++ Assets/Parralax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    public Transform alvo;
+    public float velocidadeRelativa;
+
+    void Start()
+    {
+        if (velocidadeRelativa < 1)
+            velocidadeRelativa = 1;
+
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        
+        
+    }
+}
